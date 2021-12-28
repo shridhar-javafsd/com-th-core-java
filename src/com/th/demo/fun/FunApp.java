aa89131832690324591c4631c3e7a7bbfe08e3be
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-28 10:26:03 +0530

    Update FunApp.java

--- src/com/th/demo/fun/FunApp.java
+++ src/com/th/demo/fun/FunApp.java
@@ -1,17 +1,34 @@
 package com.th.demo.fun;
 
-public class FunApp implements FunIn {
+// functional method usage - 1. using implements 
+// applicable to any interface and also to functional interface 
+//public class FunApp implements FunIn {
+//
+//	public static void main(String[] args) {
+//		FunApp obj = new FunApp();
+//		obj.functionalMethod();
+//	}
+//
+//	@Override
+//	public void functionalMethod() {
+//		System.out.println("functionalMethod implemented");
+//	}
+//}
+
+//functional method usage - 2. using implements 
+//applicable to any interface and also to functional interface 
+public class FunApp {
 
 	public static void main(String[] args) {
 
-		FunApp obj = new FunApp();
+		FunIn obj = new FunIn() {
+
+			@Override
+			public void functionalMethod() {
+				System.out.println("functionalMethod implemented");
+			}
+		};
+
 		obj.functionalMethod();
-
 	}
-
-	@Override
-	public void functionalMethod() {
-		System.out.println("functionalMethod implemented");
-	}
-
 }
