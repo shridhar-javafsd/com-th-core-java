2ef6de7262c264dde8af98b5cc35f2c1a14b5b96
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-28 10:35:28 +0530

    Update FunApp.java

--- src/com/th/demo/fun/FunApp.java
+++ src/com/th/demo/fun/FunApp.java
@@ -15,18 +15,31 @@ package com.th.demo.fun;
 //	}
 //}
 
-//functional method usage - 2. using implements 
+//functional method usage - 2. using annonymous inner class 
 //applicable to any interface and also to functional interface 
+//public class FunApp {
+//
+//	public static void main(String[] args) {
+//
+//		FunIn obj = new FunIn() {
+//
+//			public void functionalMethod() {
+//				System.out.println("aaa");
+//			}
+//		};
+//
+//		obj.functionalMethod();
+//	}
+//}
+
+//functional method usage - 3. using lambda expression 
+//applicable only to functional interface 
 public class FunApp {
 
 	public static void main(String[] args) {
-
-		FunIn obj = new FunIn() {
-
-			@Override
-			public void functionalMethod() {
-				System.out.println("functionalMethod implemented");
-			}
+		
+		FunIn obj = () -> {
+			System.out.println("functionalMethod implemented");
 		};
 
 		obj.functionalMethod();
