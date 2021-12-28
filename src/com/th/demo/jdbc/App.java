f8a259e7578c4d5673526bd02238c6c1a7a38d1a
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-28 14:46:22 +0530

    C

--- src/com/th/demo/jdbc/App.java
+++ src/com/th/demo/jdbc/App.java
@@ -15,15 +15,20 @@ public class App {
 		System.out.println("Start");
 
 		String className = "com.mysql.jdbc.Driver";
-		String url = "jdbc:mysql:localhost:3306/sys";
-		String sql = "";
+		String url = "jdbc:mysql://localhost:3306/sys";
+		String sql = "SELECT * FROM dep_table";
 		String user = "root";
 		String password = "root";
 
 		Class.forName(className);
 		Connection con = DriverManager.getConnection(url, user, password);
 		Statement st = con.createStatement();
-//		ResultSet rs = st.executeQuery(sql);
+		ResultSet rs = st.executeQuery(sql);
+		
+		while(rs.next()) {
+			System.out.println(rs.getString(1) + " " + rs.getString(2));
+			
+		}
 
 		System.out.println("End");
 
